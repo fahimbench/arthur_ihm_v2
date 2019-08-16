0946c0542cb120e910203204008cfc1d22c9372b
Benchaabane Fahim <f.benchaabane7@gmail.com>
2019-08-16 03:09:00 +0200

    Ajout page de reconnexion auto
    Pour prevenir la reco à l'aide du token refresh

---
+++ src/views/rc.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="rc-contenu">
+        <div class="rc-content">
+            <span>Votre jeton de connexion a expiré ou a été corrompu ... heureusement pour vous qu'il y avait un jeton de reconnexion.</span>
+            <span>Veuillez patienter vous allez être redirigé dans {{ time }} secondes !</span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import router from "../_helpers/router";
+
+    export default {
+        data () {
+            return {
+                startTime: 5
+            }
+        },
+        methods: {
+            decrease() {
+                this.startTime = this.startTime - 1
+                if(this.startTime === 0){
+                    router.push('/')
+                }
+            }
+        },
+        computed: {
+            time() {
+                return this.startTime
+            }
+        },
+        created() {
+            let vm = this;
+            this.$toasted.show('hello billo', { type: 'info', theme:'outline', icon: 'check_box',  duration : 5000})
+            setInterval(function(){ vm.decrease() }, 1000);
+        }
+
+
+    }
+</script>
