e7f119744b79fe457b440dbfbd3854ce2e60490b
Benchaabane Fahim <f.benchaabane7@gmail.com>
2019-08-16 13:39:55 +0200

    Suppression watch route racine

--- src/main.js
+++ src/main.js
@@ -2,21 +2,22 @@ import  Vue  from  'vue'
 import  App  from  './App.vue'
 import  router  from './_helpers/router'
 import  store from './_store/index'
-import  axios  from  'axios'
+import Toasted from 'vue-toasted'
+
+
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {authService} from "./_services/auth.service";
+import 'material-design-icons/iconfont/material-icons.css'
+import './assets/css'
 
+
+
+Vue.use(Toasted)
 Vue.config.productionTip  =  false
-Vue.prototype.$http  =  axios;
+
 
 new  Vue({
-  router,
   store,
-  watch: {
-    '$route': function (to, from) {
-      authService.testRoute(to, from)
-    }
-  },
+  router,
   render: h => h(App)
 }).$mount('#app')
